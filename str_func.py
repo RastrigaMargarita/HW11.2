5ff43408d9e759f2880725e923ccd855e3374d13
Margarita Rastriga <rasm@1Ci.com>
2023-04-23 02:49:02 +0300

    uppercase function

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def uppercase_string(word):
     """returns uppercase word changing"""
     return word.upper()
+
+
+def camelcase_string(word):
+    """returns camelcase word """
+    return word.capitalize()
